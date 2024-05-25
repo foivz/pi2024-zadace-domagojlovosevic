a6767ee167edafa20e1bc7cba5dfe649091b975e
dlovosevi <dlovosevi22@foi.hr>
2024-05-25 21:26:35 +0200

    Created login form

---
+++ Software/VarazdinBus/VarazdinBus/Login.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace VarazdinBus
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
+            this.label_username = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.White;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.ForeColor = System.Drawing.Color.Black;
+            this.txtUsername.Location = new System.Drawing.Point(88, 98);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(261, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.ForeColor = System.Drawing.Color.Black;
+            this.txtPassword.Location = new System.Drawing.Point(88, 151);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(261, 20);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(88, 192);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(261, 36);
+            this.button_login.TabIndex = 5;
+            this.button_login.Text = "Prijava";
+            this.button_login.UseVisualStyleBackColor = true;
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(160, 271);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(115, 23);
+            this.button_close.TabIndex = 6;
+            this.button_close.Text = "Odustani";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Location = new System.Drawing.Point(85, 82);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(75, 13);
+            this.label_username.TabIndex = 7;
+            this.label_username.Text = "Korisničko ime";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(85, 135);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(44, 13);
+            this.label_password.TabIndex = 8;
+            this.label_password.Text = "Lozinka";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 306);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_username);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Login";
+            this.Text = "Prijava";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Label label_username;
+        private System.Windows.Forms.Label label_password;
+    }
+}
+
--- Software/VarazdinBus/VarazdinBus/Login.cs
+++ Software/VarazdinBus/VarazdinBus/Login.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace VarazdinBus
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
