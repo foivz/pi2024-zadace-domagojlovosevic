d97fadb7d0b21b92a47d14efc1f2f1406e78c554
domagojlovosevic <dlovosevi22@student.foi.hr>
2024-05-26 19:56:44 +0200

    Implementirane funkcije za dohvaćanje stanica

---
+++ Software/VarazdinBus/VarazdinBus/Models/Stanica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarazdinBus.Models
+{
+    public class Stanica
+    {
+        public int id_stanice { get; set; }
+        public string nazivStanice { get; set; }
+        public string lokacijaStanice { get; set; }
+    }
+}
---
+++ Software/VarazdinBus/VarazdinBus/Repositories/StanicaRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using VarazdinBus.Models;
+using DBLayer;
+
+namespace VarazdinBus.Repositories
+{
+    public class StanicaRepository
+    {
+        public static List<Stanica> GetStanice()
+        {
+            var stanice = new List<Stanica>();
+
+            string sql = "SELECT * FROM dbo.Stanica";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Stanica stanica = CreateStanica(reader);
+                stanice.Add(stanica);
+            }
+            reader.Close();
+            DB.CloseConnection();
+
+            return stanice;
+        }
+
+        private static Stanica CreateStanica(SqlDataReader reader)
+        {
+            int id_stanice = Convert.ToInt32(reader["id_stanice"]);
+            string nazivStanice = Convert.ToString(reader["nazivStanice"]);
+            string lokacijaStanice = Convert.ToString(reader["lokacijaStanice"]);
+
+            return new Stanica
+            {
+                id_stanice = id_stanice,
+                nazivStanice = nazivStanice,
+                lokacijaStanice = lokacijaStanice
+            };
+        }
+    }
+}
