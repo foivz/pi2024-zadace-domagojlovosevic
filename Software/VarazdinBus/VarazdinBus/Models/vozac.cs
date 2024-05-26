650ca04c7bbfa8c5ae345a4cb882a0eb2de37efa
domagojlovosevic <dlovosevi22@student.foi.hr>
2024-05-26 17:49:38 +0200

    Implementiran repository i klasa za vozača

---
+++ Software/VarazdinBus/VarazdinBus/Models/vozac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarazdinBus.Models
+{
+    public class Vozac
+    {
+        public int vozacId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string imeVozaca { get; set; }
+        public string prezimeVozaca { get; set; }
+        public string dozvola { get; set; }
+        public DateTime datumRodjenja { get; set; }
+        public DateTime datumPocetka { get; set; }
+    }
+}
---
+++ Software/VarazdinBus/VarazdinBus/Repositories/VozacRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBLayer;
+using VarazdinBus.Models;
+
+namespace VarazdinBus.Repositories
+{
+    public class VozacRepository
+    {
+        public static Vozac GetVozac(string username)
+        {
+            string sql = $"SELECT * FROM dbo.Vozaci WHERE Username ='{username}'";
+            return FetchVozac(sql);
+        }
+
+        private static Vozac FetchVozac(string sql)
+        {
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            Vozac vozac = null;
+            if (reader.HasRows == true)
+            {
+                reader.Read();
+                vozac = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return vozac;
+        }
+
+        private static Vozac CreateObject(SqlDataReader reader)
+        {
+            int vozacId = int.Parse(reader["vozac_id"].ToString());
+            string username = reader["username"].ToString();
+            string password = reader["password"].ToString();
+            string imeVozaca = reader["ime_vozaca"].ToString();
+            string prezimeVozaca = reader["prezime_vozaca"].ToString();
+            string dozvola = reader["dozvola"].ToString();
+            DateTime datumRodjenja = DateTime.Parse(reader["datum_rodjenja"].ToString());
+            DateTime datumPocetka = DateTime.Parse(reader["datum_pocetka"].ToString());
+
+            var vozac = new Vozac
+            {
+                vozacId = vozacId,
+                username = username,
+                password = password,
+                imeVozaca = imeVozaca,
+                prezimeVozaca = prezimeVozaca,
+                dozvola = dozvola,
+                datumRodjenja = datumRodjenja,
+                datumPocetka = datumPocetka
+            };
+
+            return vozac;
+        }
+    }
+}
