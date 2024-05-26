56a27cc82dae3af1777c36d70d533e5ef1ba141d
domagojlovosevic <dlovosevi22@student.foi.hr>
2024-05-26 20:48:21 +0200

    Kreirana klasa za Liniju

---
+++ Software/VarazdinBus/VarazdinBus/Models/Linija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarazdinBus.Models
+{
+    public class Linija
+    {
+        public int IdLinije { get; set; }
+        public string NazivLinije { get; set; }
+        public string PocetnaStanica { get; set; }
+        public string ZavrsnaStanica { get; set; }
+        public DateTime VrijemePolaska { get; set; }
+        public DateTime VrijemeDolaska { get; set; }
+        public string vozaci { get; set; }
+    }
+}
