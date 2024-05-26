14751db408672cc0a532db082cd2e18004bc90a2
domagojlovosevic <dlovosevi22@student.foi.hr>
2024-05-26 18:19:20 +0200

    Implementirana klasa Djelatnik

---
+++ Software/VarazdinBus/VarazdinBus/Models/Djelatnik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarazdinBus.Models
+{
+    public class Djelatnik
+    {
+        public int djelatnikId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string imeDjelatnika { get; set; }
+        public string prezimeDjelatnika { get; set; }
+        public string radniStaz { get; set; }
+        public string datumRodjenja { get; set; }
+        public string datumPocetka { get; set; }
+    }
+}
