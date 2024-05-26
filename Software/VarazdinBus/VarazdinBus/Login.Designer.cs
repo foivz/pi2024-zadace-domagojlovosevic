d5496d3bbabebaaa3aa9d04cadcc73d264bce575
domagojlovosevic <dlovosevi22@student.foi.hr>
2024-05-26 20:50:25 +0200

    Izmjenjena početna pozicija forme na sredinu krana

--- Software/VarazdinBus/VarazdinBus/Login.Designer.cs
+++ Software/VarazdinBus/VarazdinBus/Login.Designer.cs
@@ -123,6 +123,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Prijava";
             this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
