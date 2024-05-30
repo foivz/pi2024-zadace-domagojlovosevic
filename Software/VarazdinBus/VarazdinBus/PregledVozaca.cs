09e55e0dd6a0c50656753c9f4ab71afb501d1b0b
domagojlovosevic <dlovosevi22@student.foi.hr>
2024-05-30 20:52:28 +0200

    Kreirana forma za pregled vozača

---
+++ Software/VarazdinBus/VarazdinBus/PregledVozaca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VarazdinBus
+{
+    public partial class PregledVozaca : Form
+    {
+        public PregledVozaca()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
